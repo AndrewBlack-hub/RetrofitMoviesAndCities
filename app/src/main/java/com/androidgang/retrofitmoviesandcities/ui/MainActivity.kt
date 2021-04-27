5b0e756b4f67d68c7dcd688f7b30f01eb5df17ae
AndrewBlack-hub <crys2014@mail.ru>
2021-04-27 13:44:32 +0700

    added folders and fragments

--- app/src/main/java/com/androidgang/retrofitmoviesandcities/ui/MainActivity.kt
+++ app/src/main/java/com/androidgang/retrofitmoviesandcities/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.androidgang.retrofitmoviesandcities
+package com.androidgang.retrofitmoviesandcities.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.androidgang.retrofitmoviesandcities.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
